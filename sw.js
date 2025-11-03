db4ecfdf19b5988389e290e7633c29eb9a7174fe
BLUE PAY <godswille922@gmail.com>
2025-11-03 12:26:01 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,34 @@
+// sw.js - Minimal service worker for PWA
+const CACHE_NAME = 'wonderland-chat-v1';
+const urlsToCache = [
+  '/',
+  '/friends.html',
+  '/messenger.html',
+  '/calls.html',
+  '/update.html',
+  '/community.html',
+  '/login.html',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  self.skipWaiting();
+  clients.claim();
+});
